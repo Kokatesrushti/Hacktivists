c3d85a077feae2ac7d8319bd7f5e6e65f001895a
Kokatesrushti <srushtikokate03@gmail.com>
2022-12-29 00:03:26 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString();
+const projectId = "8cc24b7b37744d9885a4199342c8e703"
+
+
+module.exports = {
+  networks: {
+    hardhat:{
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+  },
+  solidity: "0.8.17",
+};
+
